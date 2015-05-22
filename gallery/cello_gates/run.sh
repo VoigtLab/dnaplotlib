8a2f3e827850db7ad3802ad364d4e712e2f473ef
Thomas Gorochowski <tom@chofski.co.uk>
2015-05-22 08:36:20 -0400

    Updates to gallery

--- gallery/cello_gates/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python -W ignore ../../plot_SBOL_designs.py -params plot_parameters.csv -parts part_information.csv -designs dna_designs_riboj.csv -output cello_gates_riboj.pdf -regulation reg_information.csv
-python -W ignore ../../plot_SBOL_designs.py -params plot_parameters.csv -parts part_information.csv -designs dna_designs_gen1.csv -output cello_gates_gen1.pdf -regulation reg_information.csv
-python -W ignore ../../plot_SBOL_designs.py -params plot_parameters.csv -parts part_information.csv -designs dna_designs_gen2.csv -output cello_gates_gen2.pdf -regulation reg_information.csv
-python -W ignore ../../plot_SBOL_designs.py -params plot_parameters.csv -parts part_information.csv -designs dna_designs.csv -output cello_gates.pdf -regulation reg_information.csv
-

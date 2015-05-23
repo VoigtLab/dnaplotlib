d0e743bd7435f538d6b6852c1d098697ed16cd01
Thomas Gorochowski <tom@chofski.co.uk>
2015-05-23 13:22:15 -0400

    Examples update

--- gallery/order_orientation_library/run.sh
+++ gallery/order_orientation_library/run.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_PATH=../../apps
 
-python -W ignore $SCRIPT_PATH/plot_SBOL_designs.py -params 0x40_plot_parameters.csv -parts 0x40_part_information.csv -designs 0x40_dna_designs.csv -regulation 0x40_reg_information.csv -output order_orientation_library.pdf
-python -W ignore $SCRIPT_PATH/plot_SBOL_designs.py -params 0x40_plot_parameters.csv -parts 0x40_part_information.csv -designs 0x40_dna_designs.csv -regulation 0x40_reg_information.csv -output order_orientation_library.png
+python -W ignore $SCRIPT_PATH/plot_SBOL_designs.py -params plot_parameters.csv -parts part_information.csv -designs dna_designs.csv -regulation reg_information.csv -output order_orientation_library.pdf
+python -W ignore $SCRIPT_PATH/plot_SBOL_designs.py -params plot_parameters.csv -parts part_information.csv -designs dna_designs.csv -regulation reg_information.csv -output order_orientation_library.png

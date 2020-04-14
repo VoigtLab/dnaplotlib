7796a9a7ee92f6ff32898df7f3829d73dc14e7d2
Andrey Shur <andrey.shur@gmail.com>
2020-04-14 16:25:35 -0700

    Revert "revert"
    This reverts commit 86dbe50ac4e6e35098748d0b3e15a779a2f7b167.

--- dnaplotlib/dnaplotlib.py
+++ dnaplotlib/dnaplotlib.py
@@ -2421,7 +2421,7 @@ def regulation (ax, type, num, from_part, to_part, scale, linewidth, arc_height_
 	top = arcHeight;
 	base = startHeight;
 	indHeight = arrowhead_length
-	corr = linewidth
+	corr = 0*linewidth*2
 
 	if to_part['fwd'] == False:
 		base = -1*startHeight

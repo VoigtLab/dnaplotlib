cff944a7d9e9fc9d825a88425481e86af7c16c94
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2024-03-20 13:04:34 +0100

    Use pytest-mpl to test for regressions

--- gallery/sbol_visual/sbol_visual.py
+++ gallery/sbol_visual/sbol_visual.py
@@ -5,43 +5,47 @@ import dnaplotlib.sbol as dpl_sbol
 import sbol3 as sbol
 import matplotlib.pyplot as plt
 
-# Import the SBOL design file
-doc = sbol.Document()
-doc.read("gene_cassette.xml")
 
-# In this case, we know ahead of time the name of the design. In some cases, you may have to explore the doc's components to find the design you are looking for
-design = doc.find("GeneCassette")
-print(design.name)
-print(design)
+def make_plot(path_to_sbolv3: str):
+    # Import the SBOL design file
+    doc = sbol.Document()
+    doc.read(path_to_sbolv3)
 
-# Create the DNAplotlib renderer
-dr = dpl_sbol.SBOLRenderer()
+    # In this case, we know ahead of time the name of the design. In some cases, you may have to explore the doc's components to find the design you are looking for
+    design = doc.find("GeneCassette")
 
-# Instantiate rendered
-part_renderers = dr.SBOL_part_renderers()
+    # Create the DNAplotlib renderer
+    dr = dpl_sbol.SBOLRenderer()
 
-# Create the figure
-fig = plt.figure()
-ax = plt.gca()
-start, end = dr.renderSBOL(
-    ax, design, part_renderers
-)  # Render SBOL.  This function has parallel structure to renderDNA
+    # Instantiate rendered
+    part_renderers = dr.SBOL_part_renderers()
 
-# Give the figure a title
-dpl.write_label(ax, design.name, (start + end) / 2, {"label_size": 18, "label_y_offset": 12})
+    # Create the figure
+    fig = plt.figure()
+    ax = plt.gca()
+    start, end = dr.renderSBOL(
+        ax, design, part_renderers
+    )  # Render SBOL.  This function has parallel structure to renderDNA
 
-# Configure plot
-ax.set_xlim([start, end])
-ax.set_ylim([-18, 18])
-ax.set_aspect("equal")
-ax.set_xticks([])
-ax.set_yticks([])
-ax.axis("off")
+    # Give the figure a title
+    dpl.write_label(ax, design.name, (start + end) / 2, {"label_size": 18, "label_y_offset": 12})
 
-# Save the figure
-fig.savefig("sbol_visual.pdf", transparent=True)
-fig.savefig("sbol_visual.png", dpi=300)
+    # Configure plot
+    ax.set_xlim([start, end])
+    ax.set_ylim([-18, 18])
+    ax.set_aspect("equal")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.axis("off")
+    return fig, ax
 
-# Clear the plotting cache
-# plt.close('all')
-plt.show()
+
+if __name__ == "__main__":
+    fig, a_ = make_plot("gene_cassette.xml")
+    # Save the figure
+    fig.savefig("sbol_visual.pdf", transparent=True)
+    fig.savefig("sbol_visual.png", dpi=300)
+
+    # Clear the plotting cache
+    # plt.close('all')
+    plt.show()
